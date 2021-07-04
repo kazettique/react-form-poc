73cc1381c10486df90d51ebf45c2e2e062164a2c
Woody <kazettique@gmail.com>
2021-07-04 18:21:21 +0800

    Set up react router & add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import FormikTest from './pages/FormikTest';
+import RHFTest from './pages/RHFTest';
+import Home from './pages/Home';
 
-function App() {
+function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh', background: 'lightgrey', overflow: 'scroll' }}>
+      <Router>
+        <div>
+          <nav>
+            <ul style={{ margin: 0 }}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/FormikTest">FormikTest</Link>
+              </li>
+              <li>
+                <Link to="/RHFTest">RHFTest</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/FormikTest">
+              <FormikTest />
+            </Route>
+            <Route path="/RHFTest">
+              <RHFTest />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
